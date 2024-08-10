d74379bf66e595262917ea9c1f99b4eeeda1c38b
Vinh Ngo (s3578687) <41614369+vinhngogia0906@users.noreply.github.com>
2024-08-11 01:47:32 +1000

    Added CORS policy for the backend app and got the frontend app running

--- SettlementService/Program.cs
+++ SettlementService/Program.cs
@@ -7,6 +7,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Retrieve frontend URI for CORS
+var frontendUris = builder.Configuration.GetSection("Uri:Frontend").Get<string[]>() ?? throw new Exception("No frontend URIs configured.");
+
+// Add CORS
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(
+        name: "Frontend",
+        builder =>
+        {
+            builder
+                .WithOrigins(frontendUris)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +33,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS
+app.UseCors("Frontend");
 
 app.UseHttpsRedirection();
 
